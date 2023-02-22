3875b5009e421323089fa504ba2c1ead24c1ba55
josesolis-06 <124844596+josesolis-06@users.noreply.github.com>
2023-02-22 20:22:54 +0000

    tarea 3

--- numeros_pares.py
+++ numeros_pares.py
@@ -1,9 +1,3 @@
-def pares(numeros):
-    numeros_pares = []
-    for n in numeros:
-        if n % 2 ==0:
-            numeros_pares.append(n)
-            return numeros_pares
-        numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-        resultado = pares(numeros)
-        print(resultado)
+numeros = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+for numeros in range (2, 21, 2):
+    print (numeros)
